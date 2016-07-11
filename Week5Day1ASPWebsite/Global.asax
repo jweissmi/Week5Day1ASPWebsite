4d94b68dd377a756183b2ac4cf2567316a5a4fee
jweis <jweis@DESKTOP-V5M0IVC>
2016-07-11 13:10:46 -0400

    Add project files.

---
+++ Week5Day1ASPWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Week5Day1ASPWebsite.MvcApplication" Language="C#" %>
---
+++ Week5Day1ASPWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week5Day1ASPWebsite.Startup))]
+namespace Week5Day1ASPWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
