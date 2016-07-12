253651d55c871630fa68e474a10c851dcdcd89cf
Jim Weiss <jweiss@motor.com>
2016-07-11 22:13:49 -0400

    Update 4

---
+++ Week5Day1ASPWebsite/Controllers/VILController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Week5Day1ASPWebsite.Controllers
+{
+    public class VILController : Controller
+    {
+        // GET: VIL
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int id = 1)
+        {
+            //return HttpUtility.HtmlEncode("Hello my name is " + name + "; ID = " + id);
+
+            ViewBag.Name = name;
+            ViewBag.ID = id;
+
+            return View();
+
+        }
+    }
+}
